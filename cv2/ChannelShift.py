c78c90b274498daff9bf8cf3734585bdc1295713
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2021-12-16 21:35:39 +0900

    Create ChannelShift.py

---
+++ cv2/ChannelShift.py
@@ -0,0 +1,9 @@
+import ramdom
+
+def channel_shift(img, value):
+    value = int(random.uniform(-value, value))
+    img = img + value
+    img[:,:,:][img[:,:,:]>255]  = 255
+    img[:,:,:][img[:,:,:]<0]  = 0
+    #img = img.astype(np.uint8)
+    return img
